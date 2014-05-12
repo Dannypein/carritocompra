5a086bb0e7d8f21c1158187bb5cd96fc3673b76b
Kevin Perez <kevindperezm@gmail.com>
2014-05-12 18:26:25 -0500

    PDF imprimible usando printfriendly.com

--- application/views/catalogo/carrito-confirmado.php
+++ application/views/catalogo/carrito-confirmado.php
@@ -12,7 +12,32 @@
 	<div class='col-xs-12'>
 		<!-- <div class='well'> -->
 			<a class='btn btn-primary' href='<?=link_pagina($this, 'catalogo', 'catalogo')?>'>Volver al catálogo</a> 
-			<a class='btn btn-default' target="_blank" href='<?=base_url()?>carrito/imprimir/<?=$pedido->id?>'>Imprimir</a>
+			<script>
+				var pfHeaderImgUrl = '';
+				var pfHeaderTagline = '';
+				var pfdisableClickToDel = 0;
+				var pfHideImages = 0;
+				var pfImageDisplayStyle = 'block';
+				var pfDisablePDF = 0;
+				var pfDisableEmail = 0;
+				var pfDisablePrint = 0;
+				var pfCustomCSS = '';
+				var pfBtVersion='1';
+				(function(){
+					var js, pf;
+					pf = document.createElement('script');
+					pf.type = 'text/javascript';
+					if('https:' == document.location.protocol){
+						js='https://pf-cdn.printfriendly.com/ssl/main.js'
+					}else{
+						js='http://cdn.printfriendly.com/printfriendly.js'
+					}
+					pf.src=js;
+					document.getElementsByTagName('head')[0].appendChild(pf)
+				})();
+			</script>
+			<!-- <a href="http://www.printfriendly.com" style="color:#6D9F00;text-decoration:none;" class="printfriendly" onclick="window.print();return false;" title="Printer Friendly and PDF"><img style="border:none;-webkit-box-shadow:none;box-shadow:none;" src="http://cdn.printfriendly.com/button-print-gry20.png" alt="Print Friendly and PDF"/></a> -->
+			<a class='btn btn-default' target="_blank"  onclick="window.print();return false;" href='http://www.printfriendly.com'>Imprimir</a>
 		<!-- </div> -->
 	</div>
 </div>	
