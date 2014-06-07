303fa8435c33fbf2885300e01558325708609c66
Dannypein <danieldolores117@gmail.com>
2014-06-07 18:28:25 -0500

    añadidas las opciones de compra

--- application/views/acerca_de.php
+++ application/views/acerca_de.php
@@ -1,7 +1,7 @@
 <div class="col-md-12 col-sm-12 col-xs-12">
 <h1>Acerca de</h1>
 <br>
-<h3>Sitio web desarrollada por Mundo Virtual</h3>
+<h3>Sistema de carrito de compra desarrollado por Mundo Virtual</h3>
 <br>
 <p>Director de la empresa<b>  Hector Manuel Dolores Campos</b></p>
 <br>
@@ -11,10 +11,11 @@
 	<li><a>Daniel Dolores Cárdenas</a></li>
 <ul>
 <br>
-<h4>Contactenos a los correos</h4>
+<h4>Desea utilizar nuestro carrito web en su página contáctenos</h4>
 	<ul>
 		<li><a>laredmzo@hotmail.com</a></li>
 		<li><a>kevindperezm@gmail.com</a></li>
 		<li><a>danieldolores117@gmail.com</a></li>
 	</ul>
 </div>
+
